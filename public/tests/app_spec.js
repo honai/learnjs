72f2183dced5af91acd5c730ba4c176e294101f3
honai <29170100+honai@users.noreply.github.com>
2018-12-31 00:16:13 +0900

    2400:問題番号のタイトル

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,11 +1,24 @@
 describe('LearnJS', function() {
-  it('can show a problem view', function() {
+  it('問題のページを表示できる', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
   });
 
-  it('shows the landing page view when there is no hash', function() {
+  it('ハッシュがないときはランディングページを表示する', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
+  });
+
+  it('ハッシュのパラメータをview関数に投げる', function() {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+
+  describe('problem view', function() {
+    it('問題番号を含んだタイトルがある', function() {
+      var view = learnjs.problemView('1');
+      expect(view.text()).toEqual('Problem #1 Coming soon!');
+    });
   });
 });
